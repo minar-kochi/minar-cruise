7605e6be5ff22edaa7de3ced898687db3322062e
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-06-14 01:07:42 +0530

    shadcn init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <main className="">
+      <Button variant="outline">Button</Button>
+    </main>
+  );
+}
