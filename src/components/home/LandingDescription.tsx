bfa5f2fc70a84f61605abe8db8db23a7944dd287
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-06-15 04:02:24 +0530

    updated: video url

--- src/components/home/LandingDescription.tsx
+++ src/components/home/LandingDescription.tsx
@@ -16,16 +16,16 @@ const LandingDescription = () => {
     const {description,heading,image,title} = landingData
     console.log(mont.className)
   return (
-    <Bounded className="flex mt-12 justify-evenly items-center py-32">
-        <section className="max-w-lg flex flex-col gap-7">
+    <Bounded className=" lg:max-w-6xl py-12 w-full  md:place-items-center  md:grid lg:grid-cols-2 ">
+        <section className="lg:max-w-lg flex flex-col gap-7 w-full   ">
             <p className="text-sm text-red-500 font-semibold">{heading}</p>
-            <h1 className={cn("border-l-4 border-l-red-500 pl-4 text-4xl font-[Montserrat,sans-serif] font-bold ", mont.className)}>{title}</h1>
+            <h1 className={cn("border-l-4 border-l-red-500 pl-2 text-4xl font-[Montserrat,sans-serif] font-bold ", mont.className)}>{title}</h1>
             <p className="font-sans tracking-wide leading-7">{description}</p>
             <Button>Read Me</Button>
         </section>
-        <section className="p-2 " >
-            <div className="overflow-hidden rounded-full">
-                <Image width={500} height={500} src={image.url} alt={image.url} />
+        <section className="mt-12 lg:mt-0" >
+            <div className="overflow-hidden rounded-full flex items-center justify-center">
+                <Image width={500} height={500} src={image.url} style={{width: 'auto', height: 'auto'}}  className="overflow-hidden rounded-full" alt={image.url} />
             </div>
         </section>
     </Bounded>
