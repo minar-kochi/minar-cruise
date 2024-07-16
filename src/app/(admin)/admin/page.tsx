0d238af87532b9a9e45b4d2ec94034e50349d15e
muadpn <muadpn434@gmail.com>
2024-07-16 21:58:11 +0530

    Changed: text in admin dashboard page

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default async function Admin() {
   return (
     <section className="">
-      <h1 className="text-center font-bold py-5">Welcome to dashboard</h1>
+      <h1 className="text-center font-bold py-5">Welcome to Admin dashboard</h1>
       <article className="flex justify-center gap-4">
         <Link href={'/admin/schedule'}>
           <Button>Schedule</Button>
