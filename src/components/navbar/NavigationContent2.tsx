8ceae4dcf35bc952a160dfbc4fd16165f7447017
Neil Nicholas <neilnicholaskanat@gmail.com>
2024-07-17 22:00:42 +0530

    Fixed: hr in navbar

--- src/components/navbar/NavigationContent2.tsx
+++ src/components/navbar/NavigationContent2.tsx
@@ -1,4 +1,5 @@
 import { getPackageNavigation } from "@/db/data/dto/package";
+import { cn } from "@/lib/utils";
 
 import Link from "next/link";
 
@@ -23,11 +24,13 @@ const NavigationContents2 = async () => {
         <Link href="/" className="cursor-pointer  hover:text-red-500">
           Packages
         </Link>
-        <div className="absolute py-6 px-5 rounded-lg hidden w-[250px] bg-white  shadow-lg z-10 group-hover:block">
+        <div className="absolute pt-6 pb-3 px-5 rounded-lg hidden w-[250px] bg-white  shadow-lg z-10 group-hover:block">
           {packageDetails.map((item, i) => (
             <Link className="" key={item.id} href={`/booking/${item.slug}`}>
               <p className="hover:text-red-500 text-sm py-3">{item.title}</p>
-              <hr className="border-gray-200" />
+             <hr className={cn("border-gray-200 group-last-of-type:hidden", {
+              "hidden" : i === packageDetails.length-1 
+             })}  />
             </Link>
           ))}
         </div>
