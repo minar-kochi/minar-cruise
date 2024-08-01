385119cebb9bc55b74b0dc218c98bb453cc2eae7
muadpn <muadpn434@gmail.com>
2024-08-02 01:00:02 +0530

    Test the husky

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 export default async function Admin() {
   return (
     <section className="">
-      <h1 className="text-center font-bold py-5">Welcome to Admin dashboard</h1>
+      <h1 className="py-5 text-center font-bold">Welcome to Admin dashboard</h1>
       <article className="flex justify-center gap-4">
-        <Link href={'/admin/schedule'}>
+        <Link href={"/admin/schedule"}>
           <Button>Schedule</Button>
         </Link>
-        <Link href={'/admin/schedule'}>
+        <Link href={"/admin/schedule"}>
           <Button>Bookings</Button>
         </Link>
       </article>
     </section>
-);
+  );
 }
