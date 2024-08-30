18b05fc0322216ae80225c41042fb56e7020c029
unknown <neilnicholaskanat@gmail.com>
2024-08-31 00:39:00 +0530

    feat:(navigations): add navigation links to navbar and footer

--- src/components/home/CompanyInfoCard.tsx
+++ src/components/home/CompanyInfoCard.tsx
@@ -1,23 +1,33 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const CompanyInfoCard = ({ className }: { className: string }) => {
   return (
     <section>
-      <ul className={cn("flex flex-col gap-8 h-full ", className)}>
+      <div className={cn("flex flex-col gap-8 h-full ", className)}>
         <h2 className="max-sm:mx-auto text-2xl font-bold ">Company</h2>
-        <li className="max-sm:mx-auto font-semibold text-slate-400">
+        <Link
+          href="/about"
+          className="max-sm:mx-auto font-semibold text-slate-400"
+        >
           About Us
-        </li>
-        <li className="max-sm:mx-auto font-semibold text-slate-400">
+        </Link>
+        <Link href="/booking/premium-cruise" className="max-sm:mx-auto font-semibold text-slate-400">
           Packages
-        </li>
-        <li className="max-sm:mx-auto font-semibold text-slate-400">
+        </Link>
+        <Link
+          href="/facilities"
+          className="max-sm:mx-auto font-semibold text-slate-400"
+        >
           Facilities
-        </li>
-        <li className="max-sm:mx-auto font-semibold text-slate-400">
+        </Link>
+        <Link
+          href="/blog"
+          className="max-sm:mx-auto font-semibold text-slate-400"
+        >
           News And Blogs
-        </li>
-      </ul>
+        </Link>
+      </div>
     </section>
   );
 };
