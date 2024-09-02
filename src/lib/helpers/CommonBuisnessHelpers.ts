7c096fe0f6db91f478842687df0ae38d6b563192
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-09-02 18:26:39 +0530

    Added: Booking confirmation helper function

--- src/lib/helpers/CommonBuisnessHelpers.ts
+++ src/lib/helpers/CommonBuisnessHelpers.ts
@@ -1,4 +1,7 @@
 import { isValidMergeTimeCycle } from "../utils";
+import { VercelInviteUserEmail } from "@/components/services/EmailService";
+import { error } from "console";
+import { Resend } from "resend";
 
 export const selectFromTimeAndToTimeFromScheduleOrPackages = ({
   Packages,
@@ -49,3 +52,41 @@ export const selectFromTimeAndToTimeFromScheduleOrPackages = ({
     toTime: "",
   };
 };
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+type TSendConfirmationEmail = {
+  fromEmail?: string;
+  emailSubject?: string;
+  recipientEmail: string;
+  emailComponent: JSX.Element;
+};
+
+export async function sendConfirmationEmail({
+  emailSubject,
+  recipientEmail,
+  emailComponent,
+  fromEmail
+}: TSendConfirmationEmail) {
+  const { BUSINESS_EMAIL } = process.env;
+  if (!BUSINESS_EMAIL) {
+    return { error: "Business email not found" };
+  }
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: fromEmail ?? BUSINESS_EMAIL,
+      to: [recipientEmail],
+      subject: emailSubject ?? "Booking Confirmed!",
+      react: emailComponent,
+    });
+
+    if (error) {
+      return { error };
+    }
+
+    return data;
+  } catch (error) {
+    return { error };
+  }
+}
