92b921ae2d64305a4c7ed9e0b0690d8356ae69ff
muadpn <muadpn434@gmail.com>
2024-09-18 03:33:36 +0530

    feat: custom header to avoid content sniffing and other security measure

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ import remarkToc from "remark-toc";
 
 const nextConfig = {
   // Configure `pageExtensions` to include MDX fileszz
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   images: {
     remotePatterns: [
@@ -24,6 +24,35 @@ const nextConfig = {
         protocol: "https",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+        ],
+      },
+    ];
   },
 };
 const withMDX = createMDX({
