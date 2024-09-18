7ba1d5059ec434fb1360005dd0c8db5b1b59a356
muadpn <muadpn434@gmail.com>
2024-09-19 02:22:12 +0530

    feat: added google analytics and fixed gif file

--- src/components/home/BookingDescription.tsx
+++ src/components/home/BookingDescription.tsx
@@ -10,13 +10,20 @@ export default function BookingDescription() {
 
   return (
     <Bounded className="">
-      <div
-        className="min-h-[90vh] flex flex-col-reverse md:flex-row justify-center rounded-3xl w-full max-md:gap-5 bg-black "
-        style={{
-          background: 'url("/assets/sea2.gif")',
-          backgroundSize: "cover",
-        }}
-      >
+      <div className="min-h-[90vh] z-10 overflow-hidden relative flex flex-col-reverse md:flex-row justify-center rounded-3xl w-full max-md:gap-5 bg-black ">
+        <video
+          // className="  object-cover  w-full mx-auto h-[35rem] rounded-lg"
+          playsInline
+          preload="true"
+          muted
+          autoPlay
+          loop
+          // width={1080}
+          // height={1920}
+          className="absolute -z-10 top-0 object-cover left-0 w-full h-full"
+        >
+          <source src="/assets/Sea2.mp4" />
+        </video>
         <div className="md:w-[50%] flex flex-col items-center justify-center">
           <div className="relative w-[90%]  ">
             {/* <div className="absolute w-full h-full  bg-[radial-gradient(circle,_rgba(0,0,0,0)_20%,rgba(0,0,0,1)_100%)] pointer-events-noneborder-2  "></div> */}
--- src/context/services/HotJar.tsx
+++ src/context/services/HotJar.tsx
@@ -1,9 +1,7 @@
-"use client";
 import React from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
-
-export default function HotJar() {
+export default function GoogleAnalyticsWrapper() {
   return (
-    <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!}  />
+    <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!} />
   );
 }
