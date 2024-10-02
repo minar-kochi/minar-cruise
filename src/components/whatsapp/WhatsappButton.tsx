bb59a6ec19b78d593528ff4c3839b80c61cd03be
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-10-02 19:28:52 +0530

    Repositioned: Whatsapp logo

--- src/components/whatsapp/WhatsappButton.tsx
+++ src/components/whatsapp/WhatsappButton.tsx
@@ -9,8 +9,10 @@ export default function WhatsappButton() {
         phoneNumber="7034191993"
         accountName="Minar Cruise Cochin"
         avatar="/assets/whatsapplogo.png"
-        buttonClassName="absolute md:left-[30px] left-[10px]"
+        buttonClassName="absolute mb-14"
       />
     </div>
   );
 }
+
+// md:left-[30px] left-[10px]
