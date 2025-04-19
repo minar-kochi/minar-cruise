554a04c592f43cb636243b4b586ba578a6f4664d
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-04-19 09:44:43 +0530

    feat: added read, update, seed api trpc endpoints

--- src/server/routers/admin/blog.ts
+++ src/server/routers/admin/blog.ts
@@ -1,12 +1,92 @@
 import { INFINITE_QUERY_LIMIT } from "@/constants/config";
 import { db } from "@/db";
-import { sleep } from "@/lib/utils";
-import { BlogFormValidators } from "@/lib/validators/BlogFormValidators";
+import {
+  BlogFormUpdateValidator,
+  BlogFormValidators,
+} from "@/lib/validators/BlogFormValidators";
 import { AdminProcedure, publicProcedure, router } from "@/server/trpc";
+import { faker } from "@faker-js/faker";
+import { $Enums } from "@prisma/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 export const blog = router({
+  seedBlogs: AdminProcedure.input(
+    z.object({
+      count: z.number().nullish(),
+    }),
+  ).mutation(async ({ input: { count } }) => {
+    const ImagesWithID = await db.image.findMany({
+      take: 10,
+      select: { id: true },
+    });
+
+    type TBlogDetails = {
+      blogSlug: string;
+      author: string;
+      blogStatus: $Enums.BlogStatus;
+      content: string;
+      shortDes: string;
+      title: string;
+      imageId: string;
+    };
+    const ImageIds = ImagesWithID.map((item) => item.id);
+
+    const blogs: TBlogDetails[] = Array.from({ length: count ?? 10 }).map(
+      () => {
+        return {
+          author: faker.person.fullName(),
+          blogStatus: (() => {
+            const randomNumber = Math.floor(Math.random() * 2);
+            const status =
+              randomNumber === 0
+                ? ("PUBLISHED" as $Enums.BlogStatus)
+                : ("DRAFT" as $Enums.BlogStatus);
+            return status;
+          })(),
+          content: faker.lorem.paragraphs(10),
+          imageId: (() => {
+            const randomNumber = Math.floor(Math.random() * 10);
+            return ImageIds[randomNumber];
+          })(),
+          shortDes: faker.lorem.sentence(7),
+          title: faker.lorem.words(5),
+          blogSlug: faker.lorem.slug(2),
+        };
+      },
+    );
+
+    try {
+      const data = await db.blog.createMany({ data: blogs });
+      if (!data) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Failed to Seed blogs.",
+        });
+      }
+      return data;
+    } catch (error) {
+      console.log(error);
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "Failed to seed blogs.",
+      });
+    }
+  }),
+  fetchAllBlogsInfinityQuery: AdminProcedure
+    // .input(
+    //   z.object({ cursor: z.string().nullish(), limit: z.number().nullish() }),
+    // )
+    .query(async ({ input }) => {
+      // const { cursor, limit } = input;
+
+      const data = await db.blog.findMany({
+        // cursor: cursor ? { id: cursor } : undefined,
+        // take: limit ? limit + 1 : INFINITE_QUERY_LIMIT,
+      });
+
+      return data;
+    }),
   addBlog: AdminProcedure.input(BlogFormValidators).mutation(
     async ({ ctx, input }) => {
       const {
@@ -69,6 +149,45 @@ export const blog = router({
     return slugExists ? true : false;
   }),
 
+  updateBlog: AdminProcedure.input(BlogFormUpdateValidator).mutation(
+    async ({
+      input: {
+        author,
+        blogSlug,
+        blogStatus,
+        content,
+        id,
+        imageId,
+        shortDes,
+        title,
+      },
+    }) => {
+      const response = await db.blog.count({ where: { id } });
+      if (!response) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Unable to find requested blog for update",
+        });
+      }
+
+      const updateResponse = await db.blog.update({
+        where: {
+          id: id,
+        },
+        data: {
+          author,
+          blogSlug,
+          blogStatus,
+          content,
+          imageId,
+          shortDes,
+          title,
+        },
+      });
+      
+      return updateResponse
+    },
+  ),
   getImagesInfinity: AdminProcedure.input(
     z.object({
       limit: z.number().nullish(),
