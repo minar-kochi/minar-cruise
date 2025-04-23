1977b596b77881514f72be4177023f67dd1c8697
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-04-24 04:40:07 +0530

    refactor: removed loading state

--- src/app/(admin)/admin/image-uploader/page.tsx
+++ src/app/(admin)/admin/image-uploader/page.tsx
@@ -6,7 +6,7 @@ import { Suspense, useState } from "react";
 
 export default function ImageUpload() {
   return (
-    <Suspense fallback={<LoadingState />}>
+    <div className="">
       <HeaderTitleDescription
         title="Upload Gallery"
         description="Upload the image to your Storage and use Around package Images multiple times."
@@ -19,6 +19,6 @@ export default function ImageUpload() {
       <div className="group image-upload p-4">
         <ChooseImg showLink={true} />
       </div>
-    </Suspense>
+    </div>
   );
 }
