8729a3410bbfe8e0901ab7fc97b306613e14dd64
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-04-23 23:05:42 +0530

    fix: Modified blog card ui

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -1,38 +1,33 @@
 import Image from "next/image";
-import Bounded from "../elements/Bounded";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
-interface BlogcardType {
+interface BlogCardType {
   imgUrl: string;
   title: string;
   desc: string;
   link: string;
 }
 
-export default function BlogCard({ imgUrl, title, desc, link }: BlogcardType) {
+export default function BlogCard({ imgUrl, title, desc, link }: BlogCardType) {
   return (
-    <div className="border-[1px] border-muted max-w-[350px] rounded-xl flex flex-col h-fit">
-      <Link href={link} className="h-1/2 max-h-[250px]">
+    <Link href={link} className="">
+      <div className="relative h-[600px] max-w-[450px] rounded-3xl flex flex-col bg-muted/20 shadow-2xl hover:cursor-pointer">
         <Image
-          src={imgUrl ?? "/assets/world-map.png"}
+          src={imgUrl}
           alt="ship"
-          width={400}
-          height={300}
-          className="overflow-hidden rounded-xl"
+          width={1920}
+          height={1080}
+          className="rounded-t-3xl object-cover h-[60%]"
         />
-      </Link>
-      <div className="h-1/2 px-7 pt-9 pb-6  rounded-t-3xl ">
-        <Link
-          href={link}
-          className="text-xl font-semibold py-2 hover:text-red-500 cursor-pointer"
-        >
-          {title}
-        </Link>
-        <p className="text-muted-foreground py-2 text-base">{desc}</p>
-        <Link className="py-2 text-red-500 font-semibold text-sm" href={link}>
-          READ MORE
-        </Link>
+        <div className="h-[40%] w-full bottom-0 left-0 rounded-b-3xl p-5 flex flex-col gap-3">
+          <h1 className=" text-xl font-bold">{title.toUpperCase()}</h1>
+          <p className=" text-muted-foreground py-2 ">{desc}</p>
+          <Button variant={"underline"} className="  w-fit ml-auto">
+            Read More
+          </Button>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
