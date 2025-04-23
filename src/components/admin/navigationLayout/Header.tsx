e03526aac43e64b0a1b6a7aa0f239c1f007487fa
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-04-23 07:15:03 +0530

    fix: UI - Bread crump nav removed

--- src/components/admin/navigationLayout/Header.tsx
+++ src/components/admin/navigationLayout/Header.tsx
@@ -16,8 +16,8 @@ export default function Header() {
     <header className="flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 h-4" />
-        <Breadcrumb>
+        {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
+        {/* <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem className="hidden md:block">
               <BreadcrumbLink href="#">
@@ -29,7 +29,7 @@ export default function Header() {
               <BreadcrumbPage>Data Fetching</BreadcrumbPage>
             </BreadcrumbItem>
           </BreadcrumbList>
-        </Breadcrumb>
+        </Breadcrumb> */}
       </div>
       <div className="flex items-center px-4 gap-2">
         <ModeToggle />
