221256f59029b850949a4e689764814652b11570
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-04-24 04:31:09 +0530

    refactor: Blog card  ui

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
+import { truncateText } from "@/lib/utils";
 
 interface BlogCardType {
   imgUrl: string;
@@ -10,22 +11,26 @@ interface BlogCardType {
 }
 
 export default function BlogCard({ imgUrl, title, desc, link }: BlogCardType) {
+  
   return (
-    <Link href={link} className="">
-      <div className="relative h-[600px] max-w-[450px] rounded-3xl flex flex-col bg-muted/20 shadow-2xl hover:cursor-pointer">
+    <Link href={link} className="justify-self-center">
+      <div className="relative h-[500px] w-[390px] rounded-xl flex flex-col bg-muted/20 shadow-2xl hover:cursor-pointer">
         <Image
           src={imgUrl}
           alt="ship"
           width={1920}
           height={1080}
-          className="rounded-t-3xl object-cover h-[60%]"
+          className="rounded-t-xl object-cover h-[60%]"
         />
-        <div className="h-[40%] w-full bottom-0 left-0 rounded-b-3xl p-5 flex flex-col gap-3">
-          <h1 className=" text-xl font-bold">{title.toUpperCase()}</h1>
-          <p className=" text-muted-foreground py-2 ">{desc}</p>
-          <Button variant={"underline"} className="  w-fit ml-auto">
+        <div className="h-[40%] w-full rounded-b-xl flex flex-col px-4 py-2">
+          <div className="space-y-1">
+            <h1 className="  text-xl font-bold">{truncateText(title,35).toUpperCase()}</h1>
+            <p className="  text-muted-foreground">{truncateText(desc,70)}</p>
+          </div>
+          <Button variant={"underline"} className="w-fit ml-auto py-0 ">
             Read More
           </Button>
+
         </div>
       </div>
     </Link>
