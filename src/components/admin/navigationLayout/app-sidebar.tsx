698ed502bd7683ff40179c7749b9603d47ae7253
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-04-27 20:40:58 +0530

    refactor: sidebar ui

--- src/components/admin/navigationLayout/app-sidebar.tsx
+++ src/components/admin/navigationLayout/app-sidebar.tsx
@@ -14,13 +14,25 @@ import {
 } from "@/components/ui/sidebar";
 import OpenScheduleButton from "../dashboard/Schedule/OpenScheduleButton";
 import { sideBarData } from "./navigation-details";
-
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+import { signOut } from "next-auth/react";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { logo: Logo } = sideBarData.teams[0];
+
   return (
     <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <TeamSwitcher teams={sideBarData.teams} />
+      <SidebarHeader className="flex flex-row gap-2 p-5">
+        <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground ">
+          <Logo className="size-4" />
+        </div>
+
+        <div className="flex text-left  items-center justify-center font-bold text-2xl">
+          {sideBarData.teams[0].name}
+          {/* <span className="truncate text-xs">{sideBarData.teams[0].plan}</span> */}
+        </div>
+        {/* <TeamSwitcher teams={sideBarData.teams} /> */}
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={sideBarData.navMain} />
@@ -30,7 +42,16 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <div className="">
           <OpenScheduleButton />
         </div>
-        <NavUser user={sideBarData.user} />
+        {/* <NavUser user={sideBarData.user} /> */}
+        {/* <LogoutButton/> */}
+        <Button
+          onClick={() => {
+            toast.success("See you back soon!");
+            signOut({ redirect: true, callbackUrl: "/" });
+          }}
+        >
+          Logout
+        </Button>
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
