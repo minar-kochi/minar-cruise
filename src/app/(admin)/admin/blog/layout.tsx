6d8bee880a9978b5da148a85b4949ac9cfea6068
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-05-01 21:19:45 +0530

    added: parallel  and fallback route for viewing blog on blog list

---
+++ src/app/(admin)/admin/blog/layout.tsx
@@ -0,0 +1,15 @@
+export default function layout({
+  children,
+  modal,
+}: {
+    children: string;
+    modal: string;
+  }) {
+  return (
+    <div>
+      {children}
+      {modal}
+      <div id="modal-root" />
+    </div>
+  );
+}
