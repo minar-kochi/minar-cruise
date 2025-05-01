1648aef76b59fe200b1c5d85f94f0c1b1ac53271
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-05-01 13:43:27 +0530

    feat: block schedule trpc route handler, helper functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@
 import { TMeridianCycle, TSplitedFormatedDate, TTimeCycle } from "@/Types/type";
 import { $Enums } from "@prisma/client";
 import { type ClassValue, clsx } from "clsx";
-import { formatISO, isSameMonth } from "date-fns";
+import { addDays, formatISO, isBefore, isEqual, isSameMonth, startOfDay } from "date-fns";
 import moment from "moment";
 import { twMerge } from "tailwind-merge";
 import { DateTime } from "luxon";
@@ -141,6 +141,31 @@ export const sleep = (ms: number) => {
   return new Promise((r) => setTimeout(r, ms));
 };
 
+export function getDateRangeArray({
+  fromDate,
+  toDate,
+}: {
+  fromDate: Date;
+  toDate: Date;
+}) {
+  const dates = [];
+  
+  // Ensure we're working with the start of each day
+  const startDate = startOfDay(addDays(fromDate,1));
+  const endDate = startOfDay(addDays(toDate,1));
+  
+  // Initialize current date as the start date
+  let currentDate = startDate;
+  
+  // Keep adding dates until we reach or pass the end date
+  while (isBefore(currentDate, endDate) || isEqual(currentDate, endDate)) {
+    dates.push(new Date(currentDate));
+    currentDate = addDays(currentDate, 1);
+  }
+  
+  return dates;
+}
+
 export const getUTCDate = (dateStr: string): number => {
   const date = new Date(dateStr);
   return Date.UTC(
@@ -274,13 +299,13 @@ export function checkBookingTimeConstraint({
     selectedDate,
     startFrom,
   );
-  
+
   if (!UTCISTDATE) {
-    console.log("FALSE BAD STATE")
+    console.log("FALSE BAD STATE");
     return false;
   }
   const timeGap = UTCISTDATE.LuxObj.diffNow("hour").hours;
-  
+
   if (isStatusBreakfast(scheduleTime)) {
     return timeGap > MIN_BREAKFAST_BOOKING_HOUR;
   }
@@ -407,7 +432,7 @@ export const safeTotal = (value: number) => {
  */
 export function truncateText(text: string, maxLength: number): string {
   if (text.length > maxLength) {
-    return text.substring(0, maxLength) + '...';
+    return text.substring(0, maxLength) + "...";
   }
   return text;
 }
