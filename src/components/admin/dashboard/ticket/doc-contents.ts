db1403efdbf411b826b49228118b72171f1fee45
Amjuz <amjupm3@gmail.com>
2025-08-03 19:05:45 +0530

    image returns null. fix

--- src/components/admin/dashboard/ticket/doc-contents.ts
+++ src/components/admin/dashboard/ticket/doc-contents.ts
@@ -1,29 +1,32 @@
-import { ImageRun, Paragraph, TextRun } from "docx";
+import { ImageRun, Paragraph } from "docx";
 
 const imageUrl = "/assets/documents/QR.png";
+
 async function getImageBuffer() {
   const image = await fetch(imageUrl);
   const imageArrayBuffer = await image.arrayBuffer();
   return imageArrayBuffer;
 }
 
-const imageBuffer = await (async () => await getImageBuffer())();
+// const imageBuffer = await (async () => await getImageBuffer())();
 
-export const QrCode = new Paragraph({
-  children: [
-    new ImageRun({
-      type: "png",
-      data: imageBuffer,
-      transformation: {
-        width: 80,
-        height: 80,
-      },
-      altText: {
-        name: "Minar QR code",
-      },
-    }),
-    new TextRun("yo yo"),
-  ],
-  alignment: "right",
-  //   border: this.createBorder(),
-});
+export const CreateQrCode = async () =>
+  await new Paragraph({
+    children: [
+      new ImageRun({
+        type: "png",
+        data: await (async () => {
+          const imageBuffer = await getImageBuffer();
+          return imageBuffer;
+        })(),
+        transformation: {
+          width: 80,
+          height: 80,
+        },
+        altText: {
+          name: "Minar QR code",
+        },
+      }),
+    ],
+    alignment: "right",
+  });
